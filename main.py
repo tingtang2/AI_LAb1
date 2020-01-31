5cbc5ca6fafb97175e3f32eedfc0f9d14587d69b
YanishR <yr8vy@mathcs01.richmond.edu>
2020-01-31 09:52:33 -0500

    did stuff

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-### Main file to simulate the Traffic Jam puzzle ###
-### Ting Chen and Yanish Rambocus ###
-### Lab 1 CMSC 395 Artificial Intelligence ###
+### Main file to simulate the Traffic Jam puzzle
+### Ting Chen and Yanish Rambocus
+### Lab 1 CMSC 395 Artificial Intelligence
+
+'''
+class Vehicle:
+
+    def __init__(self, size, direction):
+        self.size = size
+        self.direction = direction
+'''
+
+class Grid:
+    
+    def __init__(self):
+        self.grid = [[]]
+        self.redCar
+
+    
+
