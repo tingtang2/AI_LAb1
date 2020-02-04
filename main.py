3dbefb3903566aca465b698bd57c7058a5a7ef52
tc2vg <tc2vg@mathcs03.richmond.edu>
2020-02-03 22:58:43 -0500

    started some stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 ### Ting Chen and Yanish Rambocus
 ### Lab 1 CMSC 395 Artificial Intelligence
 
+
 '''
 class Vehicle:
 
@@ -11,10 +12,36 @@ class Vehicle:
 '''
 
 class Grid:
-    
     def __init__(self):
-        self.grid = [[]]
-        self.redCar
+        self.grid = 6*[6*[0]]
+        self.redCar = -1
 
-    
+def A_star(grid):
+    path_cost = 0
+    frontier = {}
+    frontier[0] = grid
+    visited = set()
+
+    while not frontier.empty():
+        node = frontier.pop(0)
+
+        if goal_test(node):
+            return solution(node)
+
+        visited.add(node)
+
+        for child in actions(node):
+            if child not in visited or not in frontier.values():
+                frontier.put((g+h, node))
+            else if child in frontier.values() and :
+                replace something
+
+
+    return [] 
+
+def goal_test(state, sol_col):
+    return state[0][sol_col] == -1
+
+def solution(node):
+    return []
 
